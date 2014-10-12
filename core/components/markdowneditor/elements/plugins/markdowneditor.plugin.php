2eff07f4d43568d820ea19ff923566685f1291bb
Jan Peca <pecajan@gmail.com>
2014-10-12 19:35:57 +0200

    Renaming in plugin

--- core/components/markdowneditor/elements/plugins/markdowneditor.plugin.php
+++ core/components/markdowneditor/elements/plugins/markdowneditor.plugin.php
@@ -8,7 +8,7 @@
  */
 $corePath = $modx->getOption('markdowneditor.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/markdowneditor/');
 /** @var MarkdownEditor $markdowneditor */
-$collections = $modx->getService(
+$markdowneditor = $modx->getService(
     'markdowneditor',
     'MarkdownEditor',
     $corePath . 'model/markdowneditor/',
@@ -17,8 +17,8 @@ $collections = $modx->getService(
     )
 );
 
-$modx->loadClass('MarkdownEditorPlugin', $collections->getOption('modelPath') . 'markdowneditor/events/', true, true);
-$modx->loadClass($modx->event->name, $collections->getOption('modelPath') . 'markdowneditor/events/', true, true);
+$modx->loadClass('MarkdownEditorPlugin', $markdowneditor->getOption('modelPath') . 'markdowneditor/events/', true, true);
+$modx->loadClass($modx->event->name, $markdowneditor->getOption('modelPath') . 'markdowneditor/events/', true, true);
 
 if (class_exists($modx->event->name)) {
     $handler = new $modx->event->name($modx, $scriptProperties);
