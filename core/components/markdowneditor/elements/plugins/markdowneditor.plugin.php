57a4d228422616d33d6d7b7c76b80a8e9450defc
Jared Healy <jared.f.healy@gmail.com>
2021-04-18 10:53:26 -0600

    Prevent MarkdownEditor from loading for elements
    Added a check ahead of loading MarkdownEditor so that it doesn't conflict with Ace editor for Elements.

--- core/components/markdowneditor/elements/plugins/markdowneditor.plugin.php
+++ core/components/markdowneditor/elements/plugins/markdowneditor.plugin.php
@@ -6,6 +6,23 @@
  *
  *
  */
+
+// Check scenarios where MarkdownEditor should not be loaded
+$elementEditor = $modx->getOption('which_element_editor');
+$resourceEditor = $modx->getOption('which_editor');
+if ($modx->event->name === 'OnRichTextEditorInit' && !$modx->resource) {
+	// If the element editor is not MarkdownEditor, return without loading
+	if ($elementEditor !== 'MarkdownEditor') {
+		return;
+	}
+}
+else if ($modx->resource) {
+	// If MarkdownEditor is not selected, return
+	if ($resourceEditor !== 'MarkdownEditor') {
+		return;
+	}
+}
+
 $corePath = $modx->getOption('markdowneditor.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/markdowneditor/');
 /** @var MarkdownEditor $markdowneditor */
 $markdowneditor = $modx->getService(
